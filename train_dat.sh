1070afdf078152f1eb4ca3fca24f3d63c7b69643
nguyentriquan <nguyentriquan99@gmail.com>
2023-11-29 08:56:34 +0700

    dat

---
+++ train_dat.sh
@@ -0,0 +1,34 @@
+data_dir=data-bin/iwslt14.tokenized.de-en
+checkpoint_dir=/cm/archive/quannt40/Diffu-NAR/checkpoints
+
+
+fairseq-train ${data_dir}  \
+    --user-dir fs_plugins \
+    --task translation_lev_modified  --noise full_mask \
+    --arch glat_decomposed_link_base \
+    --decoder-learned-pos --encoder-learned-pos \
+    --share-all-embeddings --activation-fn gelu \
+    --apply-bert-init \
+    --links-feature feature:position --decode-strategy lookahead \
+    --max-source-positions 128 --max-target-positions 1024 --src-upsample-scale 8.0 \
+    --criterion nat_dag_loss \
+    --length-loss-factor 0 --max-transition-length 99999 \
+    --glat-p 0.5:0.1@200k --glance-strategy number-random \
+    --optimizer adam --adam-betas '(0.9,0.999)' --fp16 \
+    --label-smoothing 0.0 --weight-decay 0.01 --dropout 0.1 \
+    --lr-scheduler inverse_sqrt  --warmup-updates 10000   \
+    --clip-norm 0.1 --lr 0.0005 --warmup-init-lr '1e-07' --stop-min-lr '1e-09' \
+    --ddp-backend c10d \
+    --max-tokens 4096  --update-freq 4 --grouped-shuffling \
+    --max-update 200000 --max-tokens-valid 4096 \
+    --save-interval 1  --save-interval-updates 10000  \
+    --seed 0 \
+    --valid-subset valid \
+    --validate-interval 1       --validate-interval-updates 10000 \
+    --eval-bleu --eval-bleu-args '{"iter_decode_max_iter": 0, "iter_decode_with_beam": 1}' \
+    --eval-bleu-detok moses --eval-bleu-remove-bpe --eval-bleu-print-samples \
+    --fixed-validation-seed 7 \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric   \
+    --keep-best-checkpoints 5 --save-dir ${checkpoint_dir} \
+    --keep-interval-updates 5 --keep-last-epochs 5 \
+    --log-format 'simple' --log-interval 100
