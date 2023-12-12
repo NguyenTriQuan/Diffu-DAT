87511c2e67a9570d86dfa881a8f672eb409cee8b
nguyentriquan <nguyentriquan99@gmail.com>
2023-12-12 11:21:45 +0700

    dat

--- experiments/train_dat.sh
+++ experiments/train_dat.sh
@@ -4,7 +4,7 @@ arch=glat_decomposed_link_base
 checkpoint_dir=/cm/archive/quannt40/Diffu-DAT/checkpoints/$data_name"_"$arch
 
 
-CUDA_VISIBLE_DEVICES=5 fairseq-train ${data_dir}  \
+CUDA_VISIBLE_DEVICES=1 fairseq-train ${data_dir}  \
     --user-dir fs_plugins \
     --task translation_lev_modified  --noise full_mask \
     --arch ${arch} \
