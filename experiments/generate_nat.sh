2a9943453e695d543e6cead72625c17a111695b6
nguyentriquan <nguyentriquan99@gmail.com>
2023-12-19 02:19:28 +0700

    dat

--- experiments/generate_nat.sh
+++ experiments/generate_nat.sh
@@ -13,6 +13,6 @@ fairseq-generate ${data_dir} \
     --gen-subset test --user-dir fs_plugins --task translation_lev_modified \
     --iter-decode-max-iter 0 --iter-decode-eos-penalty 0 --beam 1 \
     --remove-bpe --max-tokens 4096 --seed 0 \
-    --model-overrides "{\"decode_strategy\":\"lookahead\"}" \
+    --decode-strategy lookahead --decode-upsample-scale 8 --decode-beta 1 \
     --path ${average_checkpoint_path} \
     --skip-invalid-size-inputs-valid-test
