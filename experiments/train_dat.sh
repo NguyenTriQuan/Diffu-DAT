6cb8fcaa1d1d99fd14b4db152a547d2ce3e04c9f
nguyentriquan <nguyentriquan99@gmail.com>
2023-12-18 10:05:43 +0700

    ard

--- experiments/train_dat.sh
+++ experiments/train_dat.sh
@@ -5,7 +5,7 @@ criterion=nat_dag_loss
 checkpoint_dir=/cm/archive/quannt40/Diffu-DAT/checkpoints_1/$data_name"_"$arch"_"$criterion
 
 
-CUDA_VISIBLE_DEVICES=1 fairseq-train ${data_dir}  \
+CUDA_VISIBLE_DEVICES=2 fairseq-train ${data_dir}  \
     --user-dir fs_plugins \
     --task translation_lev_modified  --noise full_mask \
     --arch ${arch} \
