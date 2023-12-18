da9b943bdc47eba553d582687b171c6c01992488
nguyentriquan <nguyentriquan99@gmail.com>
2023-12-19 02:26:36 +0700

    dat

--- experiments/generate_nat.sh
+++ experiments/generate_nat.sh
@@ -1,7 +1,7 @@
 data_name=iwslt14
 data_dir=data-bin/iwslt14.tokenized.de-en
 arch=glat_decomposed_link_base
-criterion=ard_dag_loss
+criterion=nat_dag_loss
 checkpoint_dir=/cm/archive/quannt40/Diffu-DAT/checkpoints/$data_name"_"$arch"_"$criterion
 average_checkpoint_path=$checkpoint_dir"/average.pt"
 
