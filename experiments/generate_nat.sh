9d3e858a6562e2c61b999c9aae20b5d518e999d6
nguyentriquan <nguyentriquan99@gmail.com>
2023-12-19 02:10:59 +0700

    dat

--- experiments/generate_nat.sh
+++ experiments/generate_nat.sh
@@ -5,6 +5,14 @@ criterion=ard_dag_loss
 checkpoint_dir=/cm/archive/quannt40/Diffu-DAT/checkpoints_1/$data_name"_"$arch"_"$criterion
 average_checkpoint_path=$checkpoint_dir"/average.pt"
 
-python3 ./fs_plugins/scripts/average_checkpoints.py --inputs ${checkpoint_dir} \
-                --max-metric --best-checkpoints-metric bleu --num-best-checkpoints-metric 5 --output ${average_checkpoint_path}
+# python3 ./fs_plugins/scripts/average_checkpoints.py --inputs ${checkpoint_dir} \
+#                 --max-metric --best-checkpoints-metric bleu --num-best-checkpoints-metric 5 --output ${average_checkpoint_path}
+
+
+fairseq-generate ${data_dir} \
+    --gen-subset test --user-dir fs_plugins --task translation_lev_modified \
+    --iter-decode-max-iter 0 --iter-decode-eos-penalty 0 --beam 1 \
+    --remove-bpe --max-tokens 4096 --seed 0 \
+    --model-overrides "{\"decode_strategy\":\"jointviterbi\",\"decode_viterbibeta\":0.1}" \
+    --path ${average_checkpoint_path}
 
