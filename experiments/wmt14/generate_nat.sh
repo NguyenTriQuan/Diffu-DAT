fad0f4fc6bf43cd12de752f58f5c13614abebfdf
nguyentriquan <nguyentriquan99@gmail.com>
2023-12-26 07:02:54 +0700

    dat

--- experiments/wmt14/generate_nat.sh
+++ experiments/wmt14/generate_nat.sh
@@ -6,15 +6,14 @@ checkpoint_dir=/cm/archive/quannt40/Diffu-DAT/wmt14/$data_name"_"$arch"_"$criter
 
 average_checkpoint_path=$checkpoint_dir"/average.pt"
 
-# python3 ./fs_plugins/scripts/average_checkpoints.py --inputs ${checkpoint_dir} \
-#                 --max-metric --best-checkpoints-metric bleu --num-best-checkpoints-metric 5 --output ${average_checkpoint_path}
+python3 ./fs_plugins/scripts/average_checkpoints.py --inputs ${checkpoint_dir} \
+                --max-metric --best-checkpoints-metric bleu --num-best-checkpoints-metric 5 --output ${average_checkpoint_path}
 
 
-CUDA_VISIBLE_DEVICES 7 fairseq-generate ${data_dir} \
+CUDA_VISIBLE_DEVICES=7 fairseq-generate ${data_dir} \
     --gen-subset test --user-dir fs_plugins --task translation_lev_modified \
     --iter-decode-max-iter 0 --iter-decode-eos-penalty 0 --beam 5 \
     --remove-bpe --max-tokens 4096 --seed 0 \
-    --model-overrides "{\"decode_strategy\":\"jointviterbi\",\"decode_viterbibeta\":1}" \
     --path ${average_checkpoint_path} \
     --skip-invalid-size-inputs-valid-test \
     --model-overrides "{\"decode_strategy\":\"jointviterbi\",\"decode_viterbibeta\":1}" \
